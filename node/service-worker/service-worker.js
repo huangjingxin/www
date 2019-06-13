25d0c6ee2480457342f14dcfa3c8a94f01396f87
huangjingxin <13824339381@139.com>
2019-06-13 21:15:03 +0800

    modify sw.js

--- node/service-worker/service-worker.js
+++ node/service-worker/service-worker.js
@@ -38,12 +38,12 @@ let cacheFiles = [
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js').then(function () {
-        // console.log('Service Worker 注册成功');
+        // console.log('1. Service Worker 注册成功');
     });
 }
 
 this.addEventListener('install', function (event) {
-    // console.log('安装 sw.js');
+    // console.log('2. 安装 sw.js');
     event.waitUntil(
         caches.open(VERSION).then(function (cache) {
             return cache.addAll(cacheFiles);
@@ -52,7 +52,7 @@ this.addEventListener('install', function (event) {
 });
 
 this.addEventListener('activate', function (event) {
-    // console.log('激活 sw.js，可以开始处理 fetch 请求。');
+    // console.log('3. 激活 sw.js，可以开始处理 fetch 请求。');
     event.waitUntil(
         caches.keys().then(function (keyList) {
             return Promise.all(keyList.map(function (key) {
@@ -67,10 +67,10 @@ this.addEventListener('activate', function (event) {
 this.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
-        .then(function (resp) {
-            if (resp) {
+        .then(function (response) {
+            if (response) {
                 // console.log('fetch ', event.request.url, '有缓存，从缓存中取');
-                return resp;
+                return response;
             } else {
                 // console.log('fetch ', event.request.url, '没有缓存，网络获取');
                 return fetch(event.request)
